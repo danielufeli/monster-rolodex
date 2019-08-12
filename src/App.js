1aef5948d315a8eb4a7de061f352f1ef2ff2a04c
Daniel Ufeli <danielufeli@yahoo.com>
2019-08-12 04:34:33 +0100

    Pushing to git

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+import "./App.css";
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+  };
+}
+
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({ monsters: users }));
+}
+
+handleChange = e => this.setState({ searchField: e.target.value });
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className='App'>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
