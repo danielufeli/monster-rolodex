1d4918e2e11cf785316e5fc31f1f2dd709032d18
Daniel Ufeli <danielufeli@yahoo.com>
2019-08-12 04:57:20 +0100

    Updates

---
+++ precache-manifest.1156f7fe0df07f7a77d904dc1bbe7d3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bcc46170ffd4bbd12472944ca86dc27",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "55d21340c81079cc2f31",
+    "url": "/monsters-rolodex/static/css/main.4dbcd7f3.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "55d21340c81079cc2f31",
+    "url": "/monsters-rolodex/static/js/main.e420d7e4.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
